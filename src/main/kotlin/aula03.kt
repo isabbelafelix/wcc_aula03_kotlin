edfc9b85553190585539ff7999d0a3fd1398a3ff
isabbelafelix <isabbelafelix@gmail.com>
2021-11-10 22:04:19 -0300

    feat: initial commit

---
+++ src/main/kotlin/aula03.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // integer, strings, character (literals)
+
+    val numero = 1_00_00
+    val texto = "Olá, como você está?"
+    val caractere = 'A'
+
+    println("numero: $numero")
+    println("texto: $texto")
+    println("caractere: $caractere")
+
+}
